22a16cec19694471f344e83d52f4f2a6933ea73c
Call Vin <kanggara75@gmail.com>
2023-07-26 00:35:09 +0700

    prepare migrate

--- WA/tests/Feature/qrcode-feature.test.js
+++ WA/tests/Feature/qrcode-feature.test.js
@@ -7,7 +7,7 @@ let QRCodeData;
 function makeid(length) {
 	let result = "";
 	const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	const charactersLength = characters.length;
 	let counter = 0;
 	while (counter < length) {
