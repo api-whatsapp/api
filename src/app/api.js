cc01483905fcb64033dbf8b57009b14955d6e498
Call Vin <kanggara75@gmail.com>
2023-08-01 16:26:55 +0700

    add: rate limiter

--- src/app/api.js
+++ src/app/api.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { limiter } from "../utils/rateLimiter.js";
 import { publicRouter } from "../routers/public-api.js";
 import { publicRouterV1 } from "../routers/routerV1.js";
 
 export const api = express();
+api.use(limiter);
 api.use(express.json());
 api.use(publicRouterV1);
 api.use(publicRouter);
