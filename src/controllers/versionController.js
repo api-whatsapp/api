4c4854d0856d68dc703dd9b51e21a62a4d5a1e6c
Call Vin <kanggara75@gmail.com>
2023-08-01 16:30:37 +0700

    fix: version auth

--- src/controllers/versionController.js
+++ src/controllers/versionController.js
@@ -3,30 +3,39 @@ import { logger } from "../app/logger.js";
 import { prismaClient } from "../app/database.js";
 
 const getVersion = async (req, res) => {
-	const bearer = req.headers.authorization.split(" ");
-	const bearerToken = bearer[1];
+	const token = req.headers.authorization;
 	const version = process.env.APP_VERSION;
+	if (token) {
+		const bearer = token.split(" ");
+		const bearerToken = bearer[1];
 
-	const tokenCheck = await prismaClient.user.findUnique({
-		where: {
-			token: bearerToken,
-		},
-		select: {
-			quota: true,
-		},
-	});
+		const tokenCheck = await prismaClient.user.findUnique({
+			where: {
+				token: bearerToken,
+			},
+			select: {
+				quota: true,
+			},
+		});
 
-	if (tokenCheck) {
-		try {
-			res.status(200).json({
-				message: "PakaiWA.my.id - UnOfficial WhatsApp API Gateway",
-				version: version,
-				stability: "Developer-Preview",
+		if (tokenCheck) {
+			try {
+				res.status(200).json({
+					message: "PakaiWA.my.id - UnOfficial WhatsApp API Gateway",
+					version: version,
+					stability: "Developer-Preview",
+				});
+				logger.info("Version check success.");
+			} catch (error) {
+				res.status(500).json({
+					message: error.message,
+				});
+				logger.error(error);
+			}
+		} else {
+			res.status(403).json({
+				message: "Invalid API token.",
 			});
-			logger.info("Version check success.");
-		} catch (error) {
-			res.status(500).json({ message: error.message });
-			logger.error(error);
 		}
 	} else {
 		res.status(401).json({
