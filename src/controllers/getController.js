021203b5f7eeb96abfb994b81ccbbcf71e204686
Call Vin <kanggara75@gmail.com>
2023-08-03 18:04:38 +0700

    fix: get device list for WA Restore

--- src/controllers/getController.js
+++ src/controllers/getController.js
@@ -1,14 +1,14 @@
-import { prismaClient } from "../app/database.js";
 import { logger } from "../app/logger.js";
+import { prismaClient } from "../app/database.js";
 
-const check = await prismaClient.hardware.findMany({
+const check = await prismaClient.device.findMany({
 	select: {
-		name: true,
+		device_id: true,
 	},
 });
 
 check.forEach(myFunction);
 
 function myFunction(item) {
-	logger.info(item["name"]);
+	logger.info(item["device_id"]);
 }
