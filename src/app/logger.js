d9b19655338acf2b6774ede1acf2b5e4e8baff2b
Call Vin <kanggara75@gmail.com>
2023-08-13 22:12:16 +0700

    Update logger config

--- src/app/logger.js
+++ src/app/logger.js
@@ -1,40 +1,62 @@
+import "dotenv/config";
 import { createLogger, format, transports } from "winston";
-const { combine, timestamp, printf, label } = format;
+const { combine, timestamp, printf } = format;
 
 //Using the printf format.
-const customFormat = printf(({ level, message, timestamp }) => {
+const logsFormat = printf(({ level, message, timestamp }) => {
 	return `"${timestamp}": {"${level}": "${message}"},`;
 });
 
 var todayDate = new Date().toISOString().slice(0, 10);
 const infoFile = todayDate + "-info.log";
+const warnFile = todayDate + "-warn.log";
 const errorFile = todayDate + "-error.log";
 const queryFile = todayDate + "-query.log";
+const appEnv = process.env.APP_ENV || "development";
+const appDebug = process.env.APP_DEBUG || "false";
+
+const myTransports = [
+	new transports.Console({
+		level: "error",
+	}),
+	new transports.File({
+		filename: "./logs/" + errorFile,
+		level: "error",
+	}),
+	new transports.File({
+		filename: "./logs/" + warnFile,
+		level: "warn",
+	}),
+	new transports.File({
+		filename: "./logs/" + infoFile,
+		level: "info",
+	}),
+	new transports.File({
+		filename: "./logs/" + queryFile,
+		level: "verbose",
+	}),
+];
+
+if (appEnv === "development" || appEnv === "dev" || appEnv === "test") {
+	myTransports.push(
+		new transports.Console({
+			level: "info",
+		}),
+		new transports.Console({
+			level: "warn",
+		})
+	);
+}
+
+if (appDebug === "true") {
+	myTransports.push(
+		new transports.Console({
+			level: "verbose",
+		})
+	);
+}
 
 export const logger = createLogger({
-	format: combine(
-		label({ label: "right meow!" }),
-		timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
-		customFormat
-	),
-	transports: [
-		new transports.Console({
-			level: "error",
-		}),
-		new transports.Console({
-			level: "info",
-		}),
-		new transports.File({
-			filename: "./logs/" + errorFile,
-			level: "error",
-		}),
-		new transports.File({
-			filename: "./logs/" + infoFile,
-			level: "info",
-		}),
-		new transports.File({
-			filename: "./logs/" + queryFile,
-			level: "verbose",
-		}),
-	],
+	format: combine(timestamp({ format: "YYYY-MM DD_HH:mm:ss.SSS" }), logsFormat),
+	transports: myTransports,
 });
