042c2741c743c8b3daa5dbc8a509d3a3336106e6
Call Vin <kanggara75@gmail.com>
2023-08-13 22:13:11 +0700

    fix: 404 test

--- tests/Feature/404.test.js
+++ tests/Feature/404.test.js
@@ -2,21 +2,22 @@ import { api } from "../../src/app/api.js";
 import supertest from "supertest";
 
 describe("404 Test", function () {
-	it("should return 404 Not Found", async function () {
-		const result = await supertest(api)
-			.get("/asmlckasm")
-			.set("Accept", "application/json")
-			.set("Authorization", "Bearer 404");
-		expect(result.status).toBe(404);
-		expect(result.body.message).toContain("Not Found");
-	});
+	for (let index = 1; index <= 5; index++) {
+		it(index + ". 404 Not Found", async function () {
+			var chars =
+				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			var charLength = chars.length;
+			var url = "";
+			for (var i = 0; i < 10; i++) {
+				url += chars.charAt(Math.floor(Math.random() * charLength));
+			}
 
-	it("should return 404 Not Found", async function () {
-		const result = await supertest(api)
-			.post("/asmlckasm")
-			.set("Accept", "application/json")
-			.set("Authorization", "Bearer 404");
-		expect(result.status).toBe(404);
-		expect(result.body.message).toContain("Not Found");
-	});
+			const result = await supertest(api)
+				.get("/" + url)
+				.set("Accept", "application/json")
+				.set("Authorization", "Bearer 404");
+			expect(result.status).toBe(404);
+			expect(result.body.message).toContain("Not Found");
+		});
+	}
 });
