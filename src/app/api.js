923a02b227403fcdbe09234bcecba2aaaa974569
Call Vin <kanggara75@gmail.com>
2023-08-18 13:42:55 +0700

    add: Response Error Handler

--- src/app/api.js
+++ src/app/api.js
@@ -3,10 +3,12 @@ import { limiter } from "../middleware/rateLimiter.js";
 import { publicRouter } from "../routers/public-api.js";
 import { publicRouterV1 } from "../routers/routerV1.js";
 import { authenticationCheck } from "../middleware/authenticationCheck.js";
+import { errorMiddleware } from "../middleware/errorMiddleware.js";
 
 export const api = express();
 api.use(authenticationCheck);
-api.use(limiter);
 api.use(express.json());
+api.use(limiter);
 api.use(publicRouterV1);
 api.use(publicRouter);
+api.use(errorMiddleware);
