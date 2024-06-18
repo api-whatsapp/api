bb4517a0a9c83ebf72b26ba56bfd88556ab09988
Call Vin <kanggara75@gmail.com>
2024-06-18 17:50:47 +0700

    add: [app] gracefulShutdown

--- src/app.js
+++ src/app.js
@@ -2,9 +2,31 @@ import "dotenv/config";
 import { app } from "./app/api.js";
 import { logger } from "./app/logger.js";
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 3030;
 global.host = process.env.API_HOST || "https://pakaiwa.my.id";
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	logger.info(`App run on port ${port}`);
 });
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
+
+function gracefulShutdown() {
+	logger.info("SIGTERM/SIGINT signal received: closing HTTP server");
+	logger.info("Shutting down gracefully...");
+
+	server.close(() => {
+		logger.info("HTTP server closed");
+		// Close any other connections or resources here
+		process.exit(0);
+	});
+
+	// Force close the server after 5 seconds
+	setTimeout(() => {
+		console.error(
+			"Could not close connections in time, forcefully shutting down"
+		);
+		process.exit(1);
+	}, 5000);
+}
