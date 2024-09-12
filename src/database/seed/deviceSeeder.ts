941898014d1b775fef3335e20ae19ef9328e248b
Call Vin <kanggara75@gmail.com>
2024-09-12 08:01:01 +0700

    fix: user_email database

--- src/database/seed/deviceSeeder.ts
+++ src/database/seed/deviceSeeder.ts
@@ -1,20 +1,20 @@
 export const deviceSeeder: {
-	userEmail: string;
+	user_email: string;
 	device_id: string;
 	phone_number: string;
 }[] = [
 	{
-		userEmail: "a@a.com",
+		user_email: "a@a.com",
 		phone_number: "112233",
 		device_id: "Hardware_1",
 	},
 	{
-		userEmail: "b@b.com",
+		user_email: "b@b.com",
 		phone_number: "123",
 		device_id: "Hardware_2",
 	},
 	{
-		userEmail: "c@c.com",
+		user_email: "c@c.com",
 		phone_number: "33321",
 		device_id: "Hardware_3",
 	},
