e1e637fa051b54f29f470cda1149ad5ae0103d63
Call Vin <kanggara75@gmail.com>
2024-09-29 16:34:02 +0700

    fix: [Server] startup with try-catch

--- src/app/server.ts
+++ src/app/server.ts
@@ -4,35 +4,28 @@ import { config } from "dotenv";
 import { logger } from "../config/logger";
 import { ExpressServer } from "./expressServer";
 import { prismaClient } from "../config/database";
-import { SessionHandler } from "../handler/sessionHandler";
 
 config();
 
 export class AppServer {
 	private server: http.Server;
 	private httpServer: ExpressServer;
-	private sessionHandler: SessionHandler;
 	private port: number = Number(process.env.PORT ?? 3030);
 
 	constructor() {
 		this.httpServer = new ExpressServer();
-		this.sessionHandler = new SessionHandler();
 		this.server = http.createServer(this.httpServer.getApp());
 	}
 
 	public async start(): Promise<void> {
-		this.server.listen(this.port, async () => {
-			try {
+		try {
+			this.server.listen(this.port, async () => {
 				logger.info(`App run on http://127.0.0.1:${this.port}`);
-
-				await this.sessionHandler
-					.init()
-					.catch((e) => logger.error(`connectToWhatsApp error: ${e}`));
-			} catch (error) {
-				logger.error(`Failed to start the server ${error}`);
-				process.exitCode = 1;
-			}
-		});
+			});
+		} catch (error) {
+			logger.error(`Failed to start the server ${error}`);
+			process.exitCode = 1;
+		}
 	}
 
 	public stop() {
